d1b37230f3f7cbacba0347ebfae45721abdeeff8
robLittiere <robinl.95@orange.fr>
2020-12-17 17:57:55 +0100

    Fixes message filter

--- config.py
+++ config.py
@@ -44,11 +44,14 @@ def get_filter_dict(ini_file):
     for effect in list_of_filters:
         effect_parameters = effect.split(':')
         effect_name = effect_parameters[0]
-        try:
-            effect_value = int(effect_parameters[1])
-            dict_filters[effect_name] = effect_value
-        except IndexError as e:
-            dict_filters[effect_name] = 0
+        if effect_name == "message":
+            dict_filters[effect_name] = effect_parameters[1]
+        else:
+            try:
+                effect_value = int(effect_parameters[1])
+                dict_filters[effect_name] = effect_value
+            except IndexError as e:
+                dict_filters[effect_name] = 0
     return dict_filters
 
 
@@ -68,3 +71,25 @@ def get_filter_list():
     for effect in list_of_filters:
         print(effect)
     print()
+
+def get_message(ini_file):
+    config_object = configparser.ConfigParser()
+    config_object.read(ini_file)
+
+    # Get a dict in the form of "content=filter"
+    content = config_object["filters"]
+
+    # Get the value from key content. Returns a string with all filters
+    filters = content["content"]
+
+    # Get all the filters inside a list
+    list_of_filters = filters.split('|')
+
+    # Get all the filters in a dict as key = name_of_filter and value = value of filter
+    dict_filters = {}
+    for effect in list_of_filters:
+        effect_parameters = effect.split(':')
+        effect_name = effect_parameters[0]
+        if effect_name == "message":
+            return dict_filters[effect_name]
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-from filters import dilateFilter, blurFilter, greyfilter, ZeTeam
+from filters import dilateFilter, blurFilter, greyfilter, zeTeam
 import directories
 import sys
 import re
@@ -63,30 +63,32 @@ for i in range(0, len(args)):
         for effect in list_effects:
             effect_parameters = effect.split(':')
             effect_name = effect_parameters[0]
+            if effect_name == "message":
+                dict_effects[effect_name] = effect_parameters[1]
+            else:
+                try:
+                    if re.match('[0-9]+', effect_parameters[1]):
+                        effect_value = int(effect_parameters[1])
 
-            try:
-                if re.match('[0-9]+', effect_parameters[1]):
-                    effect_value = int(effect_parameters[1])
+                        # Manage negative error
+                        if effect_value < 0:
+                            print(f"Expected value for {effect_name} has to be positive (>0), so we changed your value from {effect_value} to {effect_value * -1} ")
+                            effect_value *= -1
 
-                    # Manage negative error
-                    if effect_value < 0:
-                        print(f"Expected value for {effect_name} has to be positive (>0), so we changed your value from {effect_value} to {effect_value * -1} ")
-                        effect_value *= -1
+                        # Manage pair value error
+                        if effect_value % 2 == 0:
+                            print(f"Expected value for {effect_name} has to be odd, so we changed your value from {effect_value} to {effect_value + 1} ")
+                            effect_value += 1
 
-                    # Manage pair value error
-                    if effect_value % 2 == 0:
-                        print(f"Expected value for {effect_name} has to be odd, so we changed your value from {effect_value} to {effect_value + 1} ")
-                        effect_value += 1
+                        # Input values in dict as key = effect_name and value = effect_value
+                        dict_effects[effect_name] = effect_value
 
-                    # Input values in dict as key = effect_name and value = effect_value
-                    dict_effects[effect_name] = effect_value
-
-                else:
-                    print(f'Value : "{effect_parameters[1]}" for {effect_name} is incorrect. A number is required')
+                    else:
+                        print(f'Value : "{effect_parameters[1]}" for {effect_name} is incorrect. A number is required')
 
             # Handle index out of bond error
-            except IndexError as e:
-                dict_effects[effect_name] = 0
+                except IndexError as e:
+                    dict_effects[effect_name] = 0
 
 try:
     for file in files:
@@ -106,8 +108,9 @@ try:
                 if key == "grayscale":
                     greyfilter.filter(file, output_directory)
 
-                if key == "zeteam":
-                    ZeTeam.show_the_team(file, output_directory)
+                if key == "message":
+
+                    zeTeam.show_the_team(file, output_directory, dict_effects[key])
 
         except cv2.error:
             log.wrong_file(file)
